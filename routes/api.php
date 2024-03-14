2a7af4e292294920d840a97fa9716c9f551ffab9
DigoFernandes <digofernandes017@gmail.com>
2024-03-14 18:20:22 -0300

    UserResource e coll ection

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::prefix('v1')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
 });
